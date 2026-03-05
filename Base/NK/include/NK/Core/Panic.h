e7c7512b29aafda21c83a3f6403af8708cd28d7a
serial-projects <serialexperimentalchannel@gmail.com>
2026-03-05 16:53:54 -0300

    Fix the early comment on `NK/Core/Panic`.

--- Base/NK/include/NK/Core/Panic.h
+++ Base/NK/include/NK/Core/Panic.h
@@ -1,7 +1,5 @@
 /**
  * Ruka Raposa Project by Ruka Raposa Team & Contribuitors.
- * NK "New Key" is part of the Ruka Raposa Project.
- * 
  * Project is under GPL3-license, read LICENSE.md on the root.
  */
 #ifndef NKCorePanic_h
